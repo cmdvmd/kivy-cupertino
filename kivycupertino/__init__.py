dff3779becc1ef50618c07ddcbf4209a7bbdf078
cmdvmd <55916643+cmdvmd@users.noreply.github.com>
2021-02-04 13:46:58 -0500

    Added Cupertino Icon Button

---
+++ kivycupertino/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+root = os.path.dirname(__file__)
+images = os.path.join(root, 'images/')
--- kivycupertino/uix/button.py
+++ kivycupertino/uix/button.py
@@ -1,11 +1,33 @@
-from kivy.uix.behaviors.button import ButtonBehavior
+from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 from kivy.properties import StringProperty, ColorProperty
+from kivy.lang.builder import Builder
+
+Builder.load_string(f"""
+#: import images kivycupertino.__init__.images
+
+<CupertinoIconButton>:
+    canvas.before:
+        Color:
+            rgba: root.background_color
+        Rectangle:
+            size: self.size
+            pos: self.pos
+    Image:
+        source: images+root.icon+'.png'
+        size: root.size
+        pos: root.pos
+        allow_stretch: False
+        mag_filter: 'nearest'
+""")
 
 
-class SystemButton(ButtonBehavior, Label):
+class CupertinoSystemButton(ButtonBehavior, Label):
     text = StringProperty('')
     color = ColorProperty([0.1, 0.5, 1, 1])
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+
+class CupertinoIconButton(ButtonBehavior, Widget):
+    icon = StringProperty('')
+    background_color = ColorProperty([1, 1, 1, 1])
