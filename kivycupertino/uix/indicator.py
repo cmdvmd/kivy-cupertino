97fc6d68848226033f8049265440337d08558632
cmdvmd <55916643+cmdvmd@users.noreply.github.com>
2021-02-07 21:13:49 -0500

    Added CupertinoProgressBar

---
+++ kivycupertino/uix/indicator.py
@@ -0,0 +1,24 @@
+from kivy.uix.widget import Widget
+from kivy.properties import ColorProperty, NumericProperty
+from kivy.lang.builder import Builder
+
+Builder.load_string("""
+<CupertinoProgressBar>:
+    canvas.before:
+        Color:
+            rgba: root.color_unselected
+        Rectangle:
+            size: self.size
+            pos: self.pos
+        Color:
+            rgba: root.color_selected
+        Rectangle:
+            size: self.width*(root.value/100), self.height
+            pos: self.pos
+""")
+
+
+class CupertinoProgressBar(Widget):
+    color_selected = ColorProperty([0, 0.5, 1, 1])
+    color_unselected = ColorProperty([0, 0, 0, 0])
+    value = NumericProperty(0)
